04aded477bf190d1365b1ecc3e7b09c666b3f7a6
karthi <ekece94@gmail.com>
2020-01-06 18:51:40 +0530

    latest changes

--- simple.java
+++ simple.java
@@ -1,4 +1,4 @@
 public class simple{
 public static void main(String args[]){
-System.out.println("hello karthikeyan E loves nandu");
+System.out.println("hello karthikeyan E loves nandu kutty");
 }}
